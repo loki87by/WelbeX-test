4e8fa43c832cea26dbbdddd11211fa619a46e9d3
Alexey Akulich <loki1987by@yandex.ru>
2022-07-18 01:37:14 +0300

    Updates

---
+++ precache-manifest.8a011ecce291efad5aa82171f1dfb23f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260ac0441a2919d8ba8db13778fd0e8b",
+    "url": "/WelbeX-test/index.html"
+  },
+  {
+    "revision": "a9ebda10ec3cc4c0c615",
+    "url": "/WelbeX-test/static/css/main.61e38e54.chunk.css"
+  },
+  {
+    "revision": "d981fd764ad9bb23053e",
+    "url": "/WelbeX-test/static/js/2.69537a51.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/WelbeX-test/static/js/2.69537a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ebda10ec3cc4c0c615",
+    "url": "/WelbeX-test/static/js/main.08ca75ff.chunk.js"
+  },
+  {
+    "revision": "44aa346441156a601903",
+    "url": "/WelbeX-test/static/js/runtime-main.e044f83b.js"
+  }
+]);
